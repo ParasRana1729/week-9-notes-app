27810008540d2f80f73ec159c12a5af5ff9d6df7
Paras Rana <parasrana1728@gmail.com>
2026-07-04 06:21:39 +0000

    fix export bug

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,4 +1,4 @@
-function authMiddleware() {
+function authMiddleware(req, res, next) {
     const token = req.headers.token;
 
     if (!token) {
@@ -20,4 +20,4 @@ function authMiddleware() {
     next();
 }
 
-module.exports(authMiddleware);
+module.exports = authMiddleware;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const { authMiddleware } = require("./authMiddleware");
+const authMiddleware = require("./authMiddleware");
 
 const app = express();
 
